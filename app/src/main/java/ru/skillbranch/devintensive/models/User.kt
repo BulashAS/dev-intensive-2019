1082a36b1a5ad849fb937f9eb021e34d502584a5
Alexey <>
2020-04-14 01:10:35 +0700

    hometask2 start

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -2,39 +2,50 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
-class User (
+//Реализуй паттерн Factory с методом makeUser(fullName) принимающий в качесте аргумента полное имя пользователя и возвращающий экземпляр класса User
+data class User(
     val id: String,
     var firstName: String?,
     var lastName: String?,
     var avatar: String?,
-    var rating: Int=0,
-    var respect: Int=0,
-    val lastVisit: Date? = null,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    val lastVisit: Date? = Date(),
     val isOnline: Boolean = false
-){
-    constructor(id: String, firstName: String?, lastName: String?) : this(
-        id = id,
-        firstName = firstName,
-        lastName = lastName,
-        avatar = null
-    )
+) {
+    /*    constructor(id: String, firstName: String?, lastName: String?) : this(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            avatar = null
+        )
 
-    constructor(id: String) : this(id, "John", "Doe $id")
+        constructor(id: String) : this(id, "John", "Doe $id")
 
-    init {
-        println("It's Alive")
+        init {
+            println("It's Alive")
+        }
+
+        fun printMe() = println(
+            """
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            rating: $rating
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent()
+        )*/
+    companion object Factory {
+        private var lastId: Int = -1
+        fun makeUser(fullName: String?): User {
+            lastId++
+            
+        }
     }
-
-    fun printMe() = println("""
-        id: $id
-        firstName: $firstName
-        lastName: $lastName
-        avatar: $avatar
-        rating: $rating
-        respect: $respect
-        lastVisit: $lastVisit
-        isOnline: $isOnline        
-    """.trimIndent()
-    )
 }
+
+
 
